b3dc008adb501857d1cc336f727c39ae58aab90f
talal1998 <thaheemtalal@gmail.com>
2021-12-15 12:22:54 +0000

    Created Controller for Staff

--- src/main/java/com/sparta/apiproject/controllers/StaffController.java
+++ src/main/java/com/sparta/apiproject/controllers/StaffController.java
@@ -1,7 +1,12 @@
 package com.sparta.apiproject.controllers;
 
+import com.sparta.apiproject.repositories.StaffRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StaffController {
+    @Autowired
+    private StaffRepository staffRepository;
 }
